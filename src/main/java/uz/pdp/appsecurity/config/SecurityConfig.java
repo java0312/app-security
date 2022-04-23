a3d5e030183b86485274c0fc788e467e2829d43a
java0312 <aiclbwe@gmail.com>
2022-04-23 08:49:42 +0500

    second role basic

--- src/main/java/uz/pdp/appsecurity/config/SecurityConfig.java
+++ src/main/java/uz/pdp/appsecurity/config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package uz.pdp.appsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * 1. @Configuration anatatsiyasi qo'yiladi
@@ -15,18 +21,76 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration //classni bean qiladi va bu class ichida yana binlarni ochishni taminlaydi
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) //PreAuthorization ishlashi uchun
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    //1 - ochilgan metod
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
                 .csrf().disable() //serverdagi malumotlarni post, put delete qilishimiz mumkin boladi csrf bolmasa faqat get qilish mumkin
                 .authorizeRequests() //requestlarni autorizatsiya qil
+
+                /* bu yoqilsa rolelar orqali kiriladi *
+                .antMatchers(HttpMethod.GET, "/api/product/*").hasAnyRole("USER", "MANAGER", "DIRECTOR")// bitta * demak faqat bittasini olish mumkin
+                .antMatchers(HttpMethod.GET,"/api/product/*").hasAnyRole("DIRECTOR", "MANAGER") //  /api/product/ shu yolga meneger get metodi bilan murojat qilaoladi
+                .antMatchers("/api/product/*").hasRole("DIRECTOR") // /api/product dan keyingi hamma yolni DIRECTOR  qilaolsin
+                */
+
                 .anyRequest() //har qanday requestni
                 .authenticated() //requsetlarni tekshirib keyin otkazasan
                 .and()
                 .httpBasic(); //http basic bilan authentikatsiya qil, default google da form basic edi
 
     }
+
+
+
+
+
+    //2 - ochilgan metod
+    /**
+     * Foydalanuvchilarni belgilash yani authorization . va Role larni berish
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth
+                .inMemoryAuthentication()
+                .withUser("director").password(passwordEncoder().encode("director")).roles("DIRECTOR") //demak 1 ta userning username, password va rollari degan xususiyatlari bor
+                .and()
+                .withUser("manager").password(passwordEncoder().encode("manager")).roles("MANAGER")
+                .and()
+                .withUser("user").password(passwordEncoder().encode("user")).roles("USER");
+
+    }
+
+    /**
+     * Bu method passwordlarni encodlashimiz uchun
+     * @return encodlangan password
+     */
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/uz/pdp/appsecurity/controller/ProductController.java
+++ src/main/java/uz/pdp/appsecurity/controller/ProductController.java
@@ -3,11 +3,16 @@ package uz.pdp.appsecurity.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import uz.pdp.appsecurity.entity.Product;
 import uz.pdp.appsecurity.repository.ProductRepository;
 
 import java.util.Optional;
+
+/*
+*     @PreAuthorize()  metodga kirishdan oldin tekshiradi
+ * */
 
 @RestController
 @RequestMapping("/api/product")
@@ -16,16 +21,19 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+    @PreAuthorize(value = "hasAnyRole('MANAGER', 'DIRECTOR')")
     @GetMapping
     public HttpEntity<?> getAllProducts() {
         return ResponseEntity.ok(productRepository.findAll());
     }
 
+    @PreAuthorize(value = "hasRole('DIRECTOR')")
     @PostMapping
     public HttpEntity<?> addProduct(@RequestBody Product product) {
         return ResponseEntity.ok(productRepository.save(product));
     }
 
+    @PreAuthorize(value = "hasRole('DIRECTOR')")
     @PutMapping("/{id}")
     public HttpEntity<?> editProduct(@PathVariable Integer id, @RequestBody Product product) {
         Optional<Product> optionalProduct = productRepository.findById(id);
@@ -37,12 +45,14 @@ public class ProductController {
         return ResponseEntity.notFound().build();
     }
 
+    @PreAuthorize(value = "hasRole('DIRECTOR')")
     @DeleteMapping("/{id}")
     public HttpEntity<?> deleteProduct(@PathVariable Integer id){
         productRepository.deleteById(id);
         return ResponseEntity.ok().build();
     }
 
+    @PreAuthorize(value = "hasAnyRole('USER', 'MANAGER', 'DIRECTOR')")
     @GetMapping("/{id}")
     public HttpEntity<?> getProduct(@PathVariable Integer id){
         Optional<Product> optionalProduct = productRepository.findById(id);
